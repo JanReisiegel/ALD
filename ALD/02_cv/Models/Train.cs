4b7cd2986b1d31bae37c6b9c909d2a5f5c713a9d
JanReisiegel <57211451+JanReisiegel@users.noreply.github.com>
2023-10-08 19:59:55 +0200

    Train

---
+++ ALD/02_cv/Models/Train.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_cv.Models
+{
+    public class Train
+    {
+        Wagon? first, last;
+        public Train()
+        {
+            first = null;
+            last = null;
+        }
+        class Wagon
+        {
+            public string id;
+            public Wagon? prev, next;
+            public int weight;
+            public Wagon()
+            {
+                prev = null;
+                next = null;
+            }
+            public override string ToString()
+            {
+                return $"{id}:{weight}";
+            }
+        }
+
+        public string Push(string id, int wieght)
+        {
+            try
+            {
+                if (first == null)
+                {
+                    first = new Wagon();
+                    first.id = id;
+                    first.weight = wieght;
+                }
+                else if (last == null)
+                {
+                    last = new Wagon();
+                    last.id = id;
+                    last.weight = wieght;
+                    last.prev = first;
+                    first.next = last;
+                }
+                else
+                {
+                    Wagon wagon = new Wagon();
+                    wagon.id = id;
+                    wagon.weight = wieght;
+                    wagon.prev = last;
+                    last.next = wagon;
+                    last = wagon;
+                }
+                return "ok";
+            }
+            catch
+            {
+                return "error";
+            }
+        }
+
+        public string Pop(string id)
+        {
+            try
+            {
+                Wagon wagon = first;
+                while (true)
+                {
+                    if (wagon.id.Equals(id))
+                    {
+                        if (wagon.Equals(first))
+                        {
+                            first = wagon.next;
+                            first.prev = null;
+                            return "ok";
+                        }
+                        if (wagon.Equals(last))
+                        {
+                            last = wagon.prev;
+                            last.next = null;
+                            return "ok";
+                        }
+                        wagon.next.prev = wagon.prev;
+                        wagon.prev.next = wagon.next;
+                        return "ok";
+                    }
+                    wagon = wagon.next;
+                }
+            }
+            catch
+            {
+                return "error";
+            }
+        }
+
+        public string Pop(int position)
+        {
+            if(first == null)
+            {
+                return "error";
+            }
+            try
+            {
+                Wagon wagon = first;
+                for (int i = 1; i <= position; i++)
+                {
+                    if(wagon.next == null)
+                    {
+                        return "error";
+                    }
+                    wagon = wagon.next;
+                }
+                if (wagon.Equals(first))
+                {
+                    first = wagon.next;
+                    first.prev = null;
+                    return "ok";
+                }
+                if (wagon.Equals(last))
+                {
+                    last = wagon.prev;
+                    last.next = null;
+                    return "ok";
+                }
+                wagon.next.prev = wagon.prev;
+                wagon.prev.next = wagon.next;
+                return "ok";
+            }
+            catch { return "error"; }
+        }
+
+        public string Move(string id, int position) //nefunguje
+        {
+            try
+            {
+                Wagon moved, positioned;
+                moved = first;
+                while(true)
+                {
+                    if (moved == null)
+                    {
+                        return "error";
+                    }
+                    if (moved.id.Equals(id))
+                    {
+                        if(moved.prev != null)
+                        {
+                            moved.prev.next = moved.next;
+                        }
+                        if(moved.next != null)
+                        {
+                            moved.next.prev = moved.prev;
+                        }
+                        break;
+                    }
+                    moved = moved.next;
+                }
+                positioned = first;
+                for (int i = 1;i < position; i++)
+                {
+                    if (positioned.next == null)
+                    {
+                        return "error";
+                    }
+                    positioned = positioned.next;
+                    if(i == 1)
+                    {
+                        first = positioned;
+                    }
+                }
+                moved.prev = positioned;
+                moved.next = positioned.next;
+                if(positioned.next != null)
+                {
+                    positioned.next.prev = moved;
+                }
+                positioned.next = moved;
+                return "ok";
+            }
+            catch { return "error"; }
+        }
+
+        public string GetTotalWeight()
+        {
+            if(first == null)
+            {
+                return "empty";
+            }
+            int totalWeight = 0;
+            Wagon wagon = first;
+            while(true)
+            {
+                if(wagon == null)
+                {
+                    return totalWeight.ToString();
+                }
+                totalWeight += wagon.weight;
+                wagon = wagon.next;
+            }
+        }
+
+        public string PrintTrain()
+        {
+            if(first == null)
+            {
+                return "empty";
+            }
+            StringBuilder sb = new StringBuilder();
+            Wagon wagon = first;
+            while(true)
+            {
+                if(wagon == null)
+                {
+                    return sb.ToString();
+                }
+                sb.AppendLine(wagon.ToString());
+                wagon = wagon.next;
+            }
+        }
+    }
+}
--- ALD/02_cv/Program.cs
+++ ALD/02_cv/Program.cs
@@ -1,6 +1,6 @@
 ﻿using _02_cv.Models;
 
-Zasobnik<string> zs = new Zasobnik<string>();
+/*Zasobnik<string> zs = new Zasobnik<string>();
 Fronta<string> fr = new Fronta<string>();
 while (true)
 {
@@ -29,4 +29,50 @@ while (true)
         break;
     }
     Console.WriteLine(fr.Pop());
+}
+*/
+
+Train train = new Train();
+while (true)
+{
+    string input = Console.ReadLine();
+    string[] inputArgs = input.Split(' ');
+    switch(inputArgs[0])
+    {
+        case "push":
+            Console.WriteLine(train.Push(inputArgs[1], Int32.Parse(inputArgs[2])));
+            break;
+        case "pop":
+            if (inputArgs[1].StartsWith("id"))
+            {
+                string id = inputArgs[1].Split(":")[1];
+                Console.WriteLine(train.Pop(id));
+            }
+            else if (inputArgs[1].StartsWith("pos"))
+            {
+                int pos = 0;
+                try
+                {
+                    pos = Int32.Parse(inputArgs[1].Split(":")[1]);
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("error");
+                }
+                Console.WriteLine(train.Pop(pos));
+            }
+            break;
+        case "move":
+            Console.WriteLine(train.Move(inputArgs[1], Int32.Parse(inputArgs[2])));
+            break;
+        case "total_weight":
+            Console.WriteLine(train.GetTotalWeight());
+            break;
+        case "print":
+            Console.WriteLine(train.PrintTrain());
+            break;
+        default: 
+            Console.WriteLine("error");
+            break;
+    }
 }
