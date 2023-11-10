1ab40fe2ffc68ca99f19b039255780b4906df489
Jan Reisiegel <jan.reisiegel@gmail.com>
2023-11-10 14:16:56 +0100

    WordCount

---
+++ ALD/05_cv/Models/WordCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_cv.Models
+{
+    class WordCount
+    {
+        private static readonly char[] Separator = { '\n', '\r', ' ' };
+        public static async Task<string[]> ReadAll(CancellationToken ct = default)
+        {
+            var input = await Console.In.ReadToEndAsync();
+            var words = input
+                .ToLower()
+                .Split(Separator, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+        
+    }
+}
---
+++ ALD/05_cv/Program.cs
@@ -0,0 +1,49 @@
+﻿using _05_cv.Models;
+var words = await WordCount.ReadAll();
+var wordDictionary = new Dictionary<string, int>();
+words.ToList();
+foreach (var word in words)
+{
+    if (wordDictionary.ContainsKey(word))
+    {
+        wordDictionary[word]++;
+    }
+    else
+    {
+        wordDictionary.Add(word, 1);
+    }
+}
+var top10words = wordDictionary
+    .OrderByDescending(x => x.Value)
+    .Take(15)
+    .ToList();
+Console.WriteLine("Word Frequency:");
+top10words.ForEach(x =>
+{
+    var freq = (double)x.Value / words.Length;
+    Console.WriteLine( $"- {x.Key,-13} {freq,2:F}% ({x.Value})");
+});
+var frazeDictionary = new Dictionary<string, int>();
+for(int i = 1; i < words.Length - 2; i++)
+{
+    string fraze = $"{words[i-1]} {words[i]}";
+    if (frazeDictionary.ContainsKey(fraze))
+    {
+        frazeDictionary[fraze]++;
+    }
+    else
+    {
+        frazeDictionary.Add(fraze, 1);
+    }
+}
+
+var top15Fraze = frazeDictionary
+    .OrderByDescending(x => x.Value)
+    .Take(15)
+    .ToList();
+Console.WriteLine("Phrase Frequency:");
+top15Fraze.ForEach(x =>
+{
+    var freq = (double)x.Value / words.Length;
+    Console.WriteLine($"- {x.Key,-22} {freq,2:P} ({x.Value})");
+});
