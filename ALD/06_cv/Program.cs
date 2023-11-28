bac6bf1dd4c7bb35218019a5f2ce0b386799582a
JanReisiegel <57211451+JanReisiegel@users.noreply.github.com>
2023-11-28 23:53:26 +0100

    Načtení souboru do jednoho stringu

--- ALD/06_cv/Program.cs
+++ ALD/06_cv/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using _06_cv.Models;
+
+RegularExpresions reg = new RegularExpresions(@"E:/+TUL/3_semestr/ALD/Code/ALD/06_cv/vstup.html");
+Console.WriteLine(true.ToString());
