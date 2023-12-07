ac353dbb0a78927dca09d0324899a2665b5f4632
Jan Reisiegel <jan.reisiegel@gmail.com>
2023-12-07 13:33:52 +0100

    Regexdone

--- ALD/06_cv/Models/SpecializationStudents.cs
+++ ALD/06_cv/Models/SpecializationStudents.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _06_cv.Models
 {
-    internal class SpecializationStudents
+    public class SpecializationStudents
     {
         public string SpecializationName {  get; set; }
         public List<Student> Students { get; set; } = new List<Student>();
@@ -21,25 +21,28 @@ namespace _06_cv.Models
             Students.Add(student);
             Students.Sort((Student s1, Student s2) =>
             {
-                if (s1.StudentNumber % 2 == 1 && s2.StudentNumber % 2 == 0)
+                if(s1.StudentNumber % 2 == 0)
                 {
-                    return 1;
+                    if (s2.StudentNumber % 2 == 1)
+                    {
+                        return 1;
+                    }
                 }
-                else if (s1.StudentNumber.CompareTo(s2.StudentNumber) <= 0)
+                else if(s2.StudentNumber %2==0)
                 {
-                    return 1;
+                    return -1;
                 }
-                else { return 0; }
+                return s1.StudentNumber > s2.StudentNumber ? 1 : -1;
             });
         }
 
         public string PrintListOfStudents()
         {
             StringBuilder sb = new StringBuilder();
-            sb.AppendLine(SpecializationName);
-            foreach (Student student in Students)
+            sb.AppendLine(SpecializationName+":");
+            for(int i = 0; i < Students.Count; i++)
             {
-                student.PrintStudent(SpecializationName);
+                sb.AppendLine($"{i + 1, 2}: {Students[i].PrintStudent(SpecializationName)}");
             }
             return sb.ToString();
         }
--- ALD/06_cv/Models/Student.cs
+++ ALD/06_cv/Models/Student.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _06_cv.Models
 {
-    internal class Student
+    public class Student
     {
         public char StudentSpec {  get; set; }
         public int StudentNumber { get; set; }
@@ -16,14 +16,14 @@ namespace _06_cv.Models
         public Student(string studentNumber, string firstName, string lastName)
         {
             StudentSpec = studentNumber.First();
-            StudentNumber = Int16.Parse(studentNumber.Remove(0, 1));
+            StudentNumber = Int32.Parse(studentNumber.Remove(0, 1));
             FirstName = firstName;
             LastName = lastName;
         }
 
         public string PrintStudent(string specialization)
         {
-            return $"{StudentSpec} {LastName} {FirstName} {specialization} {StudentNumber}";
+            return $"{StudentSpec} {LastName.ToUpper()+" "+FirstName, -18} {specialization, -3} {StudentNumber}";
         }
     }
 }
